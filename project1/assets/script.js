58394e9f92cbb1db631f97d45938caa4bc1ff294
Miguel <galleriesproductions@gmail.com>
2024-09-12 19:34:15 -0400

    adding the temperature message feature

--- project1/assets/script.js
+++ project1/assets/script.js
@@ -32,7 +32,28 @@ function updateThermometer(tempCelsius, tempFahrenheit) {
 
     mercury.style.height = `${mercuryHeight}%`;
 
+    // Get the background color and apply it
+    function getTemperatureMessage(tempFahrenheit) {
+        if (tempFahrenheit > 100) {
+            return "Around these temperatures, it's best to stay inside.";
+        } else if (tempFahrenheit > 80) {
+            return "It's getting quite warm, stay hydrated.";
+        } else if (tempFahrenheit > 60) {
+            return "Nice and pleasant weather.";
+        } else if (tempFahrenheit > 40) {
+            return "A bit chilly, you might want a jacket.";
+        } else if (tempFahrenheit > 20) {
+            return "It's cold, stay bundled up!";
+        } else {
+            return "Convert your temperature above!";
+        }
+    }
+    const message = getTemperatureMessage(tempFahrenheit);
+    document.getElementById('changeText').textContent = message;
 }
 
 // Initialize thermometer mercury
 updateThermometer(0);
+
+
+
